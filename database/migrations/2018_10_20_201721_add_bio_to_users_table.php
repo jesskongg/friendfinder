42dae8043139329699457324ef6920d8c2d756b7
Nima Boscarino <nima.boscarino@gmail.com>
2018-10-20 13:26:28 -0700

    add bio field to users

---
+++ database/migrations/2018_10_20_201721_add_bio_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBioToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // add a bio field (STRING)
+            $table->string('bio');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // remove the bio field (STRING)
+            $table->dropColumn('bio');
+        });
+    }
+}
