fc905bb2da002a9000777ed1f7d85d4e768e5868
Nima Boscarino <nima.boscarino@gmail.com>
2018-10-27 21:48:25 -0400

    display (non-functional) "Add Friend" button on other people's profiles

--- resources/views/showprofile.blade.php
+++ resources/views/showprofile.blade.php
@@ -4,6 +4,9 @@
 </head>
 <body>
   <h1>Profile for: {{ $userRecord->name }} </h1>
+  @if(\Auth::User()->id !== $userRecord->id)
+  <button>Add Friend</button>
+  @endif
   <p>Email: {{ $userRecord->email }}<p>
   <p>Major: {{ $userRecord->major }}<p>
   <p>Minor: {{ $userRecord->minor }}<p>
