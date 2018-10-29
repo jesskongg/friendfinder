e43488a57dccbb5f317a02a7d12eefd72d5ad09d
lijason103 <lijason103@gmail.com>
2018-10-28 18:43:10 -0700

    Fixed InterestSeeder not being used

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(FriendshipsTableSeeder::class);
         $this->call(CoursesTableSeeder::class);
+        $this->call(InterestsTableSeeder::class);
     }
 }
