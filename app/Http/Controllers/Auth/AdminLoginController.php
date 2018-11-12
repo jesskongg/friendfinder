bc908d22c89c5e54e49d7d8268066f7157895b2e
Kyle <kyelmo8@gmail.com>
2018-11-12 13:44:17 -0800

    setup files for admin users

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+
+class AdminLoginController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+
+    /**
+     * Show the application’s login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('auth.admin-login');
+    }
+    // protected function guard(){
+    //     return Auth::guard('admin');
+    // }
+
+    public function login(Request $request)
+    {
+      $this->validate($request, [
+        'email' => 'required|email',
+        'password' => 'required|min:6'
+      ]);
+
+      if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+        return redirect()->intended(route('admin.dashboard'));
+      }
+
+      return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin-welcome';
+
+    public function adminLogout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        return $this->loggedOut($request) ?: redirect('/');
+    }
+
+}
