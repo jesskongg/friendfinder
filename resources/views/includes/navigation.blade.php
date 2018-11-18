610e41d7dc644f3a0b0c8514e03aa3bbe82cbc0b
lijason103 <lijason103@gmail.com>
2018-11-18 14:54:26 -0800

    Added user's name to the navbar

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -15,8 +15,8 @@
         @endif
     @else
         <a class="nav-link text-light" href="/meetups">Meetups</a>
-        <a class="nav-link text-light" href="/users/{{Auth::user()->id}}">Profile</a>
         <a class="nav-link text-light" href="/friendships">Friendships</a>
+        <a class="nav-link text-light" href="/users/{{Auth::user()->id}}">Profile ({{ Auth::user()->name }})</a>
         <a class="nav-link text-light" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             {{ __('Logout') }}
         </a>
