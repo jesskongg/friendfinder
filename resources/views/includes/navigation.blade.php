86d823b2f727bb740714d1e828118c6dc8f40edf
lijason103 <lijason103@gmail.com>
2018-11-18 15:02:35 -0800

    Added number friend requests to navbar

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -15,8 +15,8 @@
         @endif
     @else
         <a class="nav-link text-light" href="/meetups">Meetups</a>
-        <a class="nav-link text-light" href="/friendships">Friendships</a>
-        <a class="nav-link text-light" href="/users/{{Auth::user()->id}}">Profile ({{ Auth::user()->name }})</a>
+        <a class="nav-link text-light" href="/friendships">Friendships ( {{ count(Auth::user()->getFriendRequests()) }} )</a>
+        <a class="nav-link text-light" href="/users/{{Auth::user()->id}}">Profile ( {{ Auth::user()->name }} )</a>
         <a class="nav-link text-light" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             {{ __('Logout') }}
         </a>
